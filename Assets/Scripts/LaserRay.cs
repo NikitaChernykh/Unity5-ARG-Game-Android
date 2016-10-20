dd474fa7e283d9ac7d08df83b6487fb457133df4
Nikita Chernykh <chernykhmain@gmail.com>
2016-10-19 22:35:24 -0400

    Added RayCast, lineRenderer, crosshair and UI buttons

---
+++ Assets/Scripts/LaserRay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserRay : MonoBehaviour {
+	public GameObject rayOrigin;
+	private LineRenderer laserLine;
+	//private bool aimActive = true;
+	void Start () {
+		laserLine = GetComponent<LineRenderer> ();
+	}
+
+	void Update () {
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		Ray cannonRay = new Ray (rayOrigin.transform.position,fwd);
+		Debug.DrawLine (rayOrigin.transform.position,fwd,Color.green);
+		laserLine.SetPosition (0, rayOrigin.transform.position);
+		//if (aimActive) {
+		if(Physics.Raycast (cannonRay, out hit, 50)){
+			Debug.Log ("casting ray");
+		}
+			//if(hit.collider.tag == "environment"){
+				//ActivateShooting();
+			//}
+		//}
+	
+	}
+}
