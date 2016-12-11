afd40d15f5d6f443206e080c81f6ef6b386499aa
Nikita Chernykh <chernykhmain@gmail.com>
2016-12-10 23:32:26 -0500

    New shooting implementation

---
+++ Assets/Scripts/Bullet_movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_movement : MonoBehaviour {
+
+	public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject cam;
+	public GameObject shootButton;
+	public float fireRate = 1f;
+	private float nextFire;
+
+	private AudioSource cannonSound;
+
+	// Use this for initialization
+	void Start () {
+		cannonSound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	public void Shoot () {
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+
+			Destroy (Instantiate (bullet, cam.transform.position, cam.transform.rotation), 5f);
+			StartCoroutine (ShotEffect ());
+		}
+	}
+
+	private IEnumerator ShotEffect()
+	{
+		cannonSound.Play ();
+		shootButton.GetComponent<Button> ().interactable = false;
+		yield return new WaitForSeconds(fireRate); 
+		shootButton.GetComponent<Button> ().interactable = true;
+	}
+
+}
